1afeacd966fd0518a0dd2b021d02177d4cf93a28
Mathieu Duteil <mat.duteil@gmail.com>
2020-12-03 11:08:09 +0100

    Added bugs in the code, changed all minuses to +

--- Code/preprocess_data.py
+++ Code/preprocess_data.py
@@ -2,10 +2,10 @@ import numpy as np
 import pandas as pd
 
 def get_kinematics(x, y):
-	dx = x - x.shift()
-	dy = y - y.shift()
-	d2x = dx - dx.shift()
-	d2y = dy - dy.shift()
+	dx = x + x.shift()
+	dy = y + y.shift()
+	d2x = dx + dx.shift()
+	d2y = dy + dy.shift()
 
 	speed = np.sqrt(dx**2 + dy**2)
 	acceleration = np.sqrt(d2x**2 + d2y**2)
